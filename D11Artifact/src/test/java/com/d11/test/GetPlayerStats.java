4c2c7709020ea19b3774b2ca2a039b07be25bd46
sekhar638 <rajasekhar.r@invenzolabs.com>
2018-04-21 14:01:44 +0530

    5th commit

--- D11Artifact/src/test/java/com/d11/test/GetPlayerStats.java
+++ D11Artifact/src/test/java/com/d11/test/GetPlayerStats.java
@@ -1,13 +1,21 @@
 package com.d11.test;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import com.d11.page.HomePage;
 import com.d11.page.LoginPage;
 
@@ -16,10 +24,13 @@ public class GetPlayerStats extends BaseTest {
 	XSSFWorkbook workbook = new XSSFWorkbook();
 	XSSFSheet sheet = workbook.createSheet("Stats");
 	String FILE_NAME = "PlayerInfo.xlsx";
+	String READ_FILE = "PlayerTeams.txt";
+	String WRITE_FILE = "PlayerTeamsUpdated.txt";
 	String contestName = null;
 	String[] headers = null;
 	int contests = 0;
 	int rowNum = 1;
+	Map<String, String> playerTeamMap = new HashMap<String, String>();
 
 	@BeforeClass
 	public void loadPages() {
@@ -34,6 +45,44 @@ public class GetPlayerStats extends BaseTest {
 		homePage.clickResults();
 		//Index starts from 0(1), 1(2)
 		contests = homePage.getContestJoinedCount();
+
+		BufferedReader br = null;
+		FileReader fr = null;
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+
+		try {
+			fr = new FileReader(READ_FILE);
+			br = new BufferedReader(fr);
+			fw = new FileWriter(WRITE_FILE, true);
+			bw = new BufferedWriter(fw);
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				String[] data = sCurrentLine.split(":");
+				playerTeamMap.put(data[0].trim(), data[1].trim());
+			}
+			
+			for (Map.Entry<String,String> entry : playerTeamMap.entrySet()) 
+				bw.write(entry.getKey() + ":" + entry.getValue()+ "\n");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+				if (bw != null)
+					bw.close();
+				if (fw != null)
+					fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
 
 		for(int contestNo = 0; contestNo < contests; contestNo++){
 			if(contestNo != 0)
@@ -65,13 +114,15 @@ public class GetPlayerStats extends BaseTest {
 	}
 
 	private void writeToExcel(String[][] playersInfo, String contestName) {
-		int colNum = 1;
+		int colNum = 2;
 		Row row = null;
+		String playerName = null;
 		//Player Header
 		if(sheet.getPhysicalNumberOfRows() == 0) {
 			row = sheet.createRow(0);
 			Cell cell = row.createCell(0);
-			cell.setCellValue((String) "V/S");
+			cell.setCellValue((String) "VS");
+			cell.setCellValue((String) "Team");
 			for (String header : headers) {
 				cell = row.createCell(colNum++);
 				cell.setCellValue((String) header);
@@ -79,19 +130,26 @@ public class GetPlayerStats extends BaseTest {
 		}
 
 		//Player Data
-		colNum = 1;
+		colNum = 2;
 		for (String[] player : playersInfo) {
 			row = sheet.createRow(rowNum++);
 			Cell cell = row.createCell(0);
 			cell.setCellValue((String) contestName);
+			//String[] teams = contestName.split("vs");
+			
 			for (String field : player) {
 				cell = row.createCell(colNum++);
-				if(colNum == 2)
-				cell.setCellValue((String) field);
+				if(colNum == 3) {
+					cell.setCellValue((String) field);
+					playerName = field;
+				}
 				else
-				cell.setCellValue((Double) Double.parseDouble(field));	
+					cell.setCellValue((Double) Double.parseDouble(field));	
 			}
-			colNum = 1;
+			colNum = 2;
+			
+			cell = row.createCell(1);
+			cell.setCellValue((String) playerTeamMap.get(playerName));
 		}
 	}
 }
